bfb713e6bb166a77f8053a0055126819f48ae57b
Austin Blain and Eric Raetz <austin@>
2016-10-20 16:27:39 -0700

    fix 0s and letters

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 
 var triangletype = function(side1, side2, side3) {
- if (side1 === side2 && side2 === side3) {
+  if (side1 === 0 || side2 === 0 || side3 === 0) {
+    alert("Please insert numbers greater than 0.")
+  } else if (side1 === side2 && side2 === side3) {
    return "equalateral";
  } else if (side1 === side2 && side2 !== side3 && side1 + side2 > side3 || side1 === side3 && side2 !== side3 && side1 + side3 > side2 || side2 === side3 && side2 !== side1 && side2 + side3 > side1) {
    return "isoceles";
